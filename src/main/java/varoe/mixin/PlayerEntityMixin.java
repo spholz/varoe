4e4edecb42572bb942b55b4c34e0cf8c1b9807cc
Sönke Holz <sholz8530@gmail.com>
2022-04-23 16:50:23 +0200

    Only forbid registerd players to attack entities before the game has started

--- src/main/java/varoe/mixin/PlayerEntityMixin.java
+++ src/main/java/varoe/mixin/PlayerEntityMixin.java
@@ -3,9 +3,12 @@
 
 package varoe.mixin;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import varoe.Varoe;
@@ -13,9 +16,11 @@ import varoe.VaroeData;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
+    @Shadow @Final private GameProfile gameProfile;
+
     @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isAttackable()Z"))
     private boolean isAttackable(Entity target) {
-        if (Varoe.getInstance().getGameState() != VaroeData.GameState.STARTED)
+        if (Varoe.getInstance().getRegisteredPlayers().containsKey(gameProfile) && Varoe.getInstance().getGameState() != VaroeData.GameState.STARTED)
             return false;
 
         return target.isAttackable();
