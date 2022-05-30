5cf95da593e84a473ab72859d3255c8bac6ba789
Sönke Holz <sholz8530@gmail.com>
2022-05-30 22:19:31 +0200

    Add option to ban splash potions

--- src/main/java/varoe/BannedItems.java
+++ src/main/java/varoe/BannedItems.java
@@ -4,6 +4,7 @@
 package varoe;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.PotionUtil;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,16 +14,23 @@ import java.util.ArrayList;
 public class BannedItems {
     public final ArrayList<Identifier> bannedItems;
     public final ArrayList<Identifier> bannedPotions;
+    public final ArrayList<Identifier> bannedSplashPotions;
 
     public BannedItems() {
         bannedItems = new ArrayList<>();
         bannedPotions = new ArrayList<>();
+        bannedSplashPotions = new ArrayList<>();
 
         // bannedItems.add(Registry.ITEM.getId(Items.DIAMOND_BLOCK));
         // bannedPotions.add(Registry.POTION.getId(Potions.AWKWARD));
     }
 
     public boolean isBanned(ItemStack stack) {
-        return bannedItems.contains(Registry.ITEM.getId(stack.getItem())) || bannedPotions.contains(Registry.POTION.getId(PotionUtil.getPotion(stack)));
+        var item = stack.getItem();
+        var id = Registry.ITEM.getId(stack.getItem());
+
+        return bannedItems.contains(id)
+                || (item.equals(Items.POTION) && bannedPotions.contains(Registry.POTION.getId(PotionUtil.getPotion(stack))))
+                || (item.equals(Items.SPLASH_POTION) && bannedSplashPotions.contains(Registry.POTION.getId(PotionUtil.getPotion(stack))));
     }
 }
