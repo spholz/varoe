441d4aef2c1b3cb0aafe99b4230ca7ced7bcd0b1
Sönke Holz <sholz8530@gmail.com>
2022-07-04 21:24:33 +0200

    Fix hopper extract prevention

--- src/main/java/varoe/mixin/HopperBlockEntityMixin.java
+++ src/main/java/varoe/mixin/HopperBlockEntityMixin.java
@@ -3,40 +3,20 @@
 
 package varoe.mixin;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import varoe.Varoe;
-
-import java.util.function.BooleanSupplier;
 
 @Mixin(HopperBlockEntity.class)
 public abstract class HopperBlockEntityMixin {
-    @Shadow
-    @Nullable
-    public static Inventory getInventoryAt(World world, BlockPos pos) {
-        return null;
-    }
-
-    @Inject(method = "insertAndExtract", at = @At("HEAD"), cancellable = true)
-    private static void insertAndExtract(World world, BlockPos pos, BlockState state, HopperBlockEntity blockEntity, BooleanSupplier booleanSupplier, CallbackInfoReturnable<Boolean> cir) {
-        BlockPos sourcePos = new BlockPos(pos.getX(), pos.getY() + 1.0D, pos.getZ());
-        var inv = getInventoryAt(world, sourcePos);
-
-        if (inv instanceof ChestBlockEntity) {
-            var team = Varoe.getInstance().getTeamChests().get(sourcePos);
-
-            if (team != null)
-                cir.setReturnValue(false);
-        }
+    @Inject(method = "isInventoryEmpty", at = @At("HEAD"), cancellable = true)
+    private static void isInventoryEmpty(Inventory inv, Direction facing, CallbackInfoReturnable<Boolean> cir) {
+        // prevent hoppers from extracting from blocks (items are still allowed to be collected by hoppers)
+        // injecting in getInputInventory doesn't work due to fabrics own extract logic
+        cir.setReturnValue(true);
     }
 }
